20f517ecf9919f12ddd702c0024759cc0461ab9b
ALooter <58535072+ALooter@users.noreply.github.com>
2020-05-08 23:48:15 +0300

    enemy ai start

---
+++ Assets/Looter/EnemyAI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    private Vector3 startingPosition;
+
+
+    public Vector3 GetRandomDir()
+    {
+        return new Vector3(UnityEngine.Random.Range(-1f, 1f), UnityEngine.Random.Range(-1f, 1f)).normalized;
+    }
+
+
+    private void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    private Vector3 GetRoamingPosition()
+    {
+        return startingPosition + GetRandomDir() * Random.Range(10f, 20f);
+    }
+
+
+}

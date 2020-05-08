4465a91496aef5cfc4132cae657980ed04743325
CactusOwl <cacrusowl111@gmail.com>
2020-05-09 00:37:57 +0300

    Damage script

--- Assets/Rostik/bullet.cs
+++ Assets/Rostik/bullet.cs
@@ -6,8 +6,9 @@ public class bullet : MonoBehaviour
 {
     public GameObject hitEffect;
     public GameObject desEffect;
-    public float bulletDestroyTime = 60; 
-    
+
+    public float bulletDestroyTime = 1; 
+    public int damagePoints = 10;
 
     void Update()
     {
@@ -16,8 +17,13 @@ public class bullet : MonoBehaviour
         Destroy(gameObject, bulletDestroyTime);
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    void OnTriggerEnter2D(Collider2D hitInfo)
     {
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if(enemy != null)
+        {
+            enemy.TakeDamage(damagePoints);
+        }
         //GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
         //Destroy(effect, 5f);
         Destroy(gameObject);

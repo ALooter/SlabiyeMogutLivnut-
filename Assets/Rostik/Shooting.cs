94628bc9f1a90599bd8a3e92b464aa3e2261a461
CactusOwl <cacrusowl111@gmail.com>
2020-05-08 22:13:05 +0300

    Movement, shooting

---
+++ Assets/Rostik/Shooting.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform usedBullet;
+    public GameObject bulletPrefab;
+
+    public float bulletForce = 20f;
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, usedBullet.position, usedBullet.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(usedBullet.up * bulletForce, ForceMode2D.Impulse);
+    }
+}
---
+++ Assets/Rostik/bullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public GameObject hitEffect;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject effect = Instantiate(hitEffect, transoform.position, Quaternion.identity);
+        Destroy(effect, 5f);
+        Destroy(gameObject);
+    }
+}

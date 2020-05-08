4f52b450f9b2f2bdee07ecc31a55365b5819537e
CactusOwl <cacrusowl111@gmail.com>
2020-05-08 23:34:25 +0300

    bullet destroy after time

--- Assets/Rostik/Shooting.cs
+++ Assets/Rostik/Shooting.cs
@@ -22,5 +22,12 @@ public class Shooting : MonoBehaviour
         GameObject bullet = Instantiate(bulletPrefab, usedBullet.position, usedBullet.rotation);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.AddForce(usedBullet.up * bulletForce, ForceMode2D.Impulse);
+
+        
+    }
+
+    void DestroyAfterTime() 
+    {
+
     }
 }
--- Assets/Rostik/bullet.cs
+++ Assets/Rostik/bullet.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class bullet : MonoBehaviour
 {
     public GameObject hitEffect;
+    public GameObject desEffect;
+    public float bulletDestroyTime = 60; 
+    
+
+    void Update()
+    {
+        //GameObject effect = Instantiate(desEffect, transform.position, Quaternion.identity);
+        //Destroy(effect, 5f);
+        Destroy(gameObject, bulletDestroyTime);
+    }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
-        Destroy(effect, 5f);
+        //GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        //Destroy(effect, 5f);
         Destroy(gameObject);
     }
 }

8684a2513b3f7b80df5d2dfd5adfdf8a4fea2483
Robroy123l <57477346+Robroy123l@users.noreply.github.com>
2020-05-09 09:31:11 +0300

    Damaged UI

---
+++ Assets/Robroy/HealthSystem.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HealthSystem : MonoBehaviour
+{
+    public int health;
+    public int numberofLifes;
+    public Image[] lives;
+
+    public Sprite fullLive;
+    public Sprite EmptyLife;
+
+    void Update()
+
+    {
+        if (health > numberofLifes)
+        {
+            health = numberofLifes;
+        }
+
+        for (int i = 0; i < lives.Length; i++)
+        {
+            if (i < health)
+            {
+                lives[i].sprite = fullLive;
+
+            }
+            else
+            {
+                lives[i].sprite = EmptyLife;
+            }
+
+
+
+
+            if (i < numberofLifes)
+            {
+                lives[i].enabled = true;
+            }
+            else
+            {
+                lives[i].enabled = false;
+            }
+
+        }
+
+
+
+
+
+    }
+}
+ 

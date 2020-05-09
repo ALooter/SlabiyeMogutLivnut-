9d37a663f156a30972a9a41c636d0e3d80ef1a39
CactusOwl <cacrusowl111@gmail.com>
2020-05-09 13:16:03 +0300

    AI melee attack

--- Assets/Rostik/AIShooting.cs
+++ Assets/Rostik/AIShooting.cs
@@ -8,7 +8,15 @@ public class AIShooting : MonoBehaviour
     public Rigidbody2D targetPoint;
     public GameObject bulletPrefab;
     public float bulletForce = 20f;
+
+    public Transform swordPoint;
+    public float swordRange = 0.5f;
+    public LayerMask enemyLayers;
+    public int swordDM = 10;
+
     public Transform player;
+
+    public int varAttack = 0;
 
     void Update() {
 
@@ -16,7 +24,12 @@ public class AIShooting : MonoBehaviour
         float angl = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
         targetPoint.rotation = angl;
 
-        Shoot();
+        if(varAttack == 0){
+            Shoot();
+        }
+        else if(varAttack == 1) {
+            Hit();
+        }
     }
 
     void Shoot()
@@ -24,6 +37,23 @@ public class AIShooting : MonoBehaviour
         GameObject bullet = Instantiate(bulletPrefab, shootpoint.position, shootpoint.rotation);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.AddForce(shootpoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+
+    void Hit() 
+    {
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(swordPoint.position, swordRange, enemyLayers);
+        foreach (Collider2D player in hitEnemies)
+        {
+            player.GetComponent<Stats>().TakeDamage(swordDM);
+        }
+    }
+
+    void OnDrawGizmosSelected() {
+        if(swordPoint == null)
+            return;
+
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(swordPoint.position, swordRange);
     }
 
 }
